383463edef7d8294de892982896a638d2a2bca03
Dotan Cohen <github-com-2010.04@dotancohen.com>
2015-01-18 22:50:33 +0200

    Check for proper type on dict

--- json_explorer.py
+++ json_explorer.py
@@ -80,30 +80,31 @@ def output_location(path, code):
 		print("Str Value: %s" % (current_element,))
 		return path[:-1]
 
-	for index, ce_sub in enumerate(current_element):
-		if type(ce_sub)==type([]):
-			ce_sub_type = str(type(current_element[index]))
-		else:
-			ce_sub_type = str(type(current_element[ce_sub]))
-		ce_sub_type = ce_sub_type[ce_sub_type.find("'")+1:]
-		ce_sub_type = ce_sub_type[:ce_sub_type.find("'")]
+	if type(current_element)==type({'adam':'eve'}):
+		for index, ce_sub in enumerate(current_element):
+			if type(ce_sub)==type([]):
+				ce_sub_type = str(type(current_element[index]))
+			else:
+				ce_sub_type = str(type(current_element[ce_sub]))
+			ce_sub_type = ce_sub_type[ce_sub_type.find("'")+1:]
+			ce_sub_type = ce_sub_type[:ce_sub_type.find("'")]
 
-		# Types observered: dict lit int
+			# Types observered: dict lit int
 
-		if ce_sub_type in ['dict', 'list']:
-			ce_sub_type += ' ' + str(len(current_element[ce_sub]))
+			if ce_sub_type in ['dict', 'list']:
+				ce_sub_type += ' ' + str(len(current_element[ce_sub]))
 
-		if ce_sub_type == 'str':
-			ce_sub_type += ' ' + ce_sub[:50]
-			if len(ce_sub)>50:
-				ce_sub_type += '...'
+			if ce_sub_type == 'str':
+				ce_sub_type += ' ' + ce_sub[:50]
+				if len(ce_sub)>50:
+					ce_sub_type += '...'
 
-		if ce_sub_type == 'int':
-			ce_sub_type += ' ' + str(current_element[ce_sub])
+			if ce_sub_type == 'int':
+				ce_sub_type += ' ' + str(current_element[ce_sub])
 
-		print('%2.0f: %s (%s)' % (ordinal, ce_sub, ce_sub_type,))
-		path_addition.append(ce_sub)
-		ordinal += 1
+			print('%2.0f: %s (%s)' % (ordinal, ce_sub, ce_sub_type,))
+			path_addition.append(ce_sub)
+			ordinal += 1
 
 	if not onTopLevel:
 		print('%2.0f: %s' % (0, 'Up one level.',))
